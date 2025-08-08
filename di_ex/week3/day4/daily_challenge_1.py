7ecbc9f4bf132b3cb43a601ae09e59f720bbf3f5
ehud <ehudmi12@gmail.com>
2025-08-08 12:43:50 +0300

    week 3 day 4 daily challenge

--- di_ex/week3/day4/daily_challenge_1.py
+++ di_ex/week3/day4/daily_challenge_1.py
@@ -19,6 +19,7 @@
 # Bonus (not mandatory) : Install the Turtle module, and draw the sorted circles
 
 import math
+import turtle as t
 
 
 class Circle:
@@ -27,65 +28,71 @@ class Circle:
             self.radius = kwargs["radius"]
         if "diameter" in kwargs.keys():
             self.radius = kwargs["diameter"] / 2
-        self.area = math.pi * self.radius**2
+
+    @property
+    def diameter(self):
+        return self.radius * 2
+
+    @property
+    def area(self):
+        return math.pi * self.radius**2
 
     def __str__(self):
-        return f"I am a circle and my radius is {self.radius} and my diameter is {self.radius*2} and my area is {self.area:.2f}"
+        return f"I am a circle and my radius is {self.radius} and my diameter is {self.diameter} and my area is {self.area:.2f}"
 
     def __add__(self, other):
-        if isinstance(other, Circle):
-            new_radius = self.radius + other.radius
-            new_circle = Circle(radius=new_radius)
-            return new_circle
-        else:
+        if not isinstance(other, Circle):
             raise TypeError("You are not adding a circle object")
+        new_radius = self.radius + other.radius
+        return Circle(radius=new_radius)
 
     def __gt__(self, other):
-        if isinstance(other, Circle):
-            if self.radius > other.radius:
-                return True
-            else:
-                return False
-        else:
-            raise TypeError("You are not adding a circle object")
+        if not isinstance(other, Circle):
+            raise TypeError("You are not comparing to a circle object")
+        return self.radius > other.radius
+
+    def __lt__(self, other):
+        if not isinstance(other, Circle):
+            raise TypeError("You are not comparing to a circle object")
+        return self.radius < other.radius
 
     def __eq__(self, other):
-        if isinstance(other, Circle):
-            if self.radius == other.radius:
-                return True
-            else:
-                return False
-        else:
-            raise TypeError("You are not adding a circle object")
+        if not isinstance(other, Circle):
+            raise TypeError("You are not comparing to a circle object")
+        return self.radius == other.radius
 
     @classmethod
     def sort_circles(cls, circle_list):
         """class method for sorting circles"""
         if not isinstance(circle_list, list):
             raise TypeError("You did not provide a list")
-        n = len(circle_list)
-        for i in range(n - 1):
-            swapped = False
-            for j in range(n - i - 1):
-                if not isinstance(circle_list[j], cls):
-                    raise TypeError("One of the items is not a Circle")
-                if circle_list[j] > circle_list[j + 1]:
-                    circle_list[j], circle_list[j + 1] = (
-                        circle_list[j + 1],
-                        circle_list[j],
-                    )
-                    swapped = True
-                if not swapped:
-                    break
-        return circle_list
+        for circle in circle_list:
+            if not isinstance(circle, cls):
+                raise TypeError("One of the items is not a Circle")
+        return sorted(circle_list)
+
+    @classmethod
+    def draw_circle(cls, circle_list):
+        if not isinstance(circle_list, list):
+            raise TypeError("You did not provide a list")
+        for circle in circle_list:
+            if not isinstance(circle, cls):
+                raise TypeError("One of the items is not a Circle")
+            t.penup()
+            t.goto(0, -circle.radius)  # Center the circle on the screen
+            t.pendown()
+            t.circle(circle.radius)
 
 
 circle1 = Circle(radius=5)
 circle2 = Circle(diameter=12)
+circle3 = Circle(radius=8)
+circle4 = Circle(radius=2)
 print(circle1)
 print(circle2)
 print(circle1 + circle2)
 print(circle1 > circle2)
 print(circle2 > circle1)
 # print(circle1 + 5)
-print(Circle.sort_circles([circle2, circle1]))
+sorted_list = Circle.sort_circles([circle1, circle2, circle3, circle4])
+Circle.draw_circle(sorted_list)
